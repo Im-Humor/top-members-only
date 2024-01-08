5fccdc645558b94c1e44d6f0317d0ba481e6b758
Im-Humor <orangite@gmail.com>
2024-01-08 14:24:30 -0700

    add support for login sessions and the ability to upgrade membership with given code

--- app.js
+++ app.js
@@ -2,22 +2,76 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const mongoose = require("mongoose");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
 
 //set view engine
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
+//set public directory
 app.use(express.static("public"));
+
+//set passport strategy
+passport.use(
+	new LocalStrategy(async function verify(username, password, done) {
+		try {
+			const user = await User.findOne({ email: username });
+			if (!user) {
+				return done(null, false, { message: "Incorrect username" });
+			}
+			const match = await bcrypt.compare(password, user.password);
+			if (!match) {
+				return done(null, false, { message: "Incorrect password" });
+			}
+			return done(null, user);
+		} catch (err) {
+			console.log(err);
+		}
+	})
+);
+
+app.use(
+	session({
+		secret: "keyboard cat",
+		resave: false,
+		saveUninitialized: true,
+	})
+);
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+	try {
+		const user = await User.findById(id);
+		done(null, user);
+	} catch (err) {
+		done(err);
+	}
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //connect to database
 async function main() {
 	await mongoose.connect(
 		`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.hszdb78.mongodb.net/?retryWrites=true&w=majority`
 	);
-	console.log(mongoose.connection.readyState);
 }
 
 main().catch((err) => console.log(err));
+
+// make user available to all views via locals object
+app.use((req, res, next) => {
+	res.locals.currentUser = req.user;
+	next();
+});
 
 //import routers
 const index = require("./routes/index");
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,106 @@
 const express = require("express");
 const router = express.Router();
+const valid = require("express-validator");
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
+const Post = require("../models/post");
+const passport = require("passport");
+
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+
+// express validation for signup form
+const createUserValidator = [
+	valid.body("first_name").notEmpty().isAlpha(),
+	valid.body("last_name").notEmpty().isAlpha(),
+	valid
+		.body("email")
+		.notEmpty()
+		.isEmail()
+		.custom(async (value) => {
+			const findEmail = await User.findOne({ email: value });
+			if (findEmail) {
+				throw new Error("Email already in use");
+			}
+		}),
+	valid.body("password").notEmpty().isAlphanumeric(),
+	valid.body("confirm_password").custom((value, { req }) => {
+		return value === req.body.password;
+	}),
+];
+
+//post method for receiving signup form information
+router.post("/", createUserValidator, (req, res) => {
+	const result = valid.validationResult(req);
+	if (result.isEmpty()) {
+		bcrypt.hash(req.body.password, 8, async (err, hash) => {
+			if (err) {
+				console.log(err);
+			} else {
+				await User.create({
+					first_name: req.body.first_name,
+					last_name: req.body.last_name,
+					email: req.body.email,
+					password: hash,
+					membership: false,
+				});
+				res.redirect("/");
+			}
+		});
+	} else {
+		res.redirect("/");
+	}
+});
 
 router.get("/", (req, res) => {
 	res.render("index", { title: "Homepage" });
+});
+
+router.get("/login", (req, res) => {
+	res.render("login", { title: "Log in" });
+});
+
+router.post(
+	"/login",
+	passport.authenticate("local", {
+		failureRedirect: "/login",
+		failureMessage: true,
+	}),
+	(req, res) => {
+		res.redirect("/");
+	}
+);
+
+router.get("/join-club", (req, res) => {
+	res.render("join-club", { title: "Upgrade Membership" });
+});
+
+router.post("/join-club", async (req, res) => {
+	if (req.body.join_code == "69420") {
+		try {
+			const user = await User.findOne({ email: req.user.email });
+			user.membership = true;
+			await user.save();
+		} catch (err) {
+			console.log(err);
+		}
+		res.redirect("/");
+	} else {
+		res.redirect("/join-club");
+	}
+});
+
+router.get("/account", (req, res) => {
+	res.render("account", { title: "Account" });
+});
+
+router.get("/logout", (req, res, next) => {
+	req.logout((err) => {
+		if (err) {
+			return next(err);
+		}
+		res.redirect("/");
+	});
 });
 
 module.exports = router;
